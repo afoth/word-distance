7dbb49098ce0a005c87e9cdcbfdfec3192c074b8
Anton F <ijon57@gmail.com>
2018-08-26 16:06:01 +0200

    added main function for command line execution

--- word_distance.py
+++ word_distance.py
@@ -1,30 +1,37 @@
 import re
+import sys
+
 
 class WordDistance:
+    def __init__(self, text):
+        # extract words from given text
+        self.words = re.compile('\w+').findall(text.lower())
 
-  def __init__(self, text):
-    # extract words from given text
-    self.words = re.compile('\w+').findall(text.lower())
+    # finds shortest distance in number of words between start and end
+    # returns -1 if start in the list and start equals end
+    # returns -1 if start or end not in the list
+    def find_shortest_distance(self, start, end):
+        start = start.lower()
+        end = end.lower()
+        min_dist = -1
+        last = -1
+        # pre-checks
+        if start == end or len(self.words) < 2:
+            return -1
+        # iterate over the list of words
+        # calculate distance of last occurrence with the current element if matching start or end
+        for i in range(len(self.words)):
+            if self.words[i] == start or self.words[i] == end:
+                dist = i - last - 1
+                if self.words[i] != self.words[last] and last >= 0 and (dist < min_dist or min_dist < 0):
+                    min_dist = dist
+                last = i
+        return min_dist
 
-  # finds shortest distance in number of words between start and end
-  # returns -1 if start in the list and start equals end
-  # returns -1 if start or end not in the list
-  def find_shortest_distance(self, start, end):
-    start = start.lower()
-    end = end.lower()
-    min_dist = -1
-    last = -1
-    # pre-checks
-    if start == end or len(self.words) < 2:
-      return -1
-    # iterate over the list of words
-    # calculate distance of last occurrence with the current element if matching start or end
-    for i in range(len(self.words)):
-        if self.words[i] == start or self.words[i] == end:
-          dist = i - last - 1
-          if self.words[i] != self.words[last] and last >= 0 and (dist < min_dist or min_dist < 0):
-            min_dist = dist
-          last = i
-    return min_dist
+def main(path, start, end):
+    content = open(path, 'r').read()
+    distance = WordDistance(content).find_shortest_distance(start, end)
+    print(f'Number of words between {start!r} and {end!r}: {distance!s}')
 
-
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
